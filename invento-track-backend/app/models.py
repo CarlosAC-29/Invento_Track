4aafc7b34d90eb4e6192984dd807343c4bdb653b
Luisa Cardenas <lcardenas@morris-labs.com>
2024-04-25 13:58:55 -0500

    edit client and seller

--- invento-track-backend/app/models.py
+++ invento-track-backend/app/models.py
@@ -33,3 +33,29 @@ class Administrador(db.Model):
     email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
     role = db.Column(db.String(128), default='admin')
+
+# Editar datos cliente
+def editar_cliente(cliente_id, nombre, apellido, email, direccion, telefono):
+    cliente = Cliente.query.get(cliente_id)
+    if cliente:
+        cliente.nombre = nombre
+        cliente.apellido = apellido
+        cliente.email = email
+        cliente.direccion = direccion
+        cliente.telefono = telefono
+        db.session.commit()
+        return True
+    return False
+
+# Editar datos vendedor
+def editar_vendedor(vendedor_id, nombre, apellido, email, estado, password):
+    vendedor = Vendedor.query.get(vendedor_id)
+    if vendedor:
+        vendedor.nombre = nombre
+        vendedor.apellido = apellido
+        vendedor.email = email
+        vendedor.estado = estado
+        vendedor.password = password
+        db.session.commit()
+        return True
+    return False
