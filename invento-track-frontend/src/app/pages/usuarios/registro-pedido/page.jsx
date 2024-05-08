7e786db8adc83320a0d999e1319c4301b31d4b4c
Carlos Caceres <carlosmicro33@gmail.com>
2024-05-08 08:17:45 -0500

    pedidos update

--- invento-track-frontend/src/app/pages/usuarios/registro-pedido/page.jsx
+++ invento-track-frontend/src/app/pages/usuarios/registro-pedido/page.jsx
@@ -1,271 +1,120 @@
 "use client"
 import React, { useState } from 'react';
-import Swal from 'sweetalert2';
-import styles from './styles.module.css'
+import { Box, Typography, Stack, TextField, Button, Autocomplete, Divider } from '@mui/material';
+import styles from './styles.module.css';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import Image from 'next/image';
-import vendorIcon from '../../../../../public/images/vendedor-icon.png';
-import { useForm } from 'react-hook-form';
-import {
-    Box,
-    Typography,
-    Stack,
-    TextField,
-    Button
-} from '@mui/material';
-import { registarVendedor } from '@/app/api/api.routes';
-import { Password } from '@mui/icons-material';
-import { useRouter } from 'next/navigation';
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAWrC4NNeZpkbsDnmFM4ZirY3uzJu_GDHA",
-    authDomain: "logininventotrack.firebaseapp.com",
-    projectId: "logininventotrack",
-    storageBucket: "logininventotrack.appspot.com",
-    messagingSenderId: "692002139318",
-    appId: "1:692002139318:web:c627ac41ca3cc4b1d64a17"
-  };
-  
-const app = initializeApp(firebaseConfig);
 
 export default function RegistroPedido() {
+    const [productosAgregados, setProductosAgregados] = useState([]);
 
-    const { register, handleSubmit} = useForm(
-        {
-            defaultValues: {
-                nombre: '',
-                apellido: '',
-                email: '',
-                password: '',
-                confPassword: ''
+    const Clientes = [
+        { value: 1, label: 'Carlos' },
+        { value: 2, label: 'Camilo' },
+        { value: 3, label: 'Luis' }
+    ];
+
+    const Productos = [
+        { value: 1, label: 'Agua', precio: 2000 },
+        { value: 2, label: 'Gaseosa', precio: 3000 },
+        { value: 3, label: 'Bombon', precio: 500 }
+    ];
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    const handleCantidadChange = (event, index) => {
+        const { value } = event.target;
+        const nuevosProductos = productosAgregados.map((producto, i) => {
+            if (i === index) {
+                const valorTotal = producto.valorUnitario * value;
+                return { ...producto, cantidad: value, valorTotal };
             }
-        }
-    )
-
-    const handleClick = () => {
-        router.push('../listas/cliente');
-      };
-
-    const router = useRouter();
-
-    const [data, setData] = useState('')
-
-    const [nombreError, setNombreError] = useState('')
-    const [apellidoError, setApellidoeError] = useState('')
-    const [passowordError, setPasswordError] = useState('')
-    const [confPasswordError, setConfPasswordError] = useState('')
-    const [emailError, setEmailError] = useState('')
-
-
-    const saveData = async (data) => {
-
-        Swal.fire({
-            title: 'Esperando respuesta del servidor...',
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            button: true,
-            showConfirmButton: false,
-            didOpen: () => {
-                Swal.showLoading();
-            }
-        })
-
-        const response = await registarVendedor(data)
-        
-        //Firebase
-        const auth = getAuth();
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, data.email, data.password);
-            // Usuario registrado con éxito
-            console.log('Usuario registrado:', userCredential.user);
-            // Aquí puedes redirigir a otra página o realizar otras acciones después del registro exitoso
-        } catch (error) {
-            // Si hay un error durante el registro
-            console.error('Error de registro:', error.message);
-        }
-
-        console.log(response)
-
-        if(response){
-            Swal.close()
-            Swal.fire({
-                title: 'Vendedor registrado con éxito',
-                icon: 'success',
-                confirmButtonText: 'Ok',
-            })
-        }else{
-            Swal.close()
-            Swal.fire({
-                title: 'Error al registrar el vendedor',
-                icon: 'error',
-                confirmButtonText: 'Ok',
-            })
-        }
-
+            return producto;
+        });
+        setProductosAgregados(nuevosProductos);
     }
 
-    const processForm = (data) => {
-        setData(data);
-        console.log(data);
     
-        let isValid = true;
-    
-        if (!data.nombre || !data.nombre.length) {
-            setNombreError('Campo requerido');
-            isValid = false;
+
+    const handleDeleteProducto = (index) => {
+        setProductosAgregados(productosAgregados.filter((producto, i) => i !== index));
+    };
+
+    const handleAddProducto = (productoSeleccionado) => {
+        const productoExistenteIndex = productosAgregados.findIndex(producto => producto.referencia === productoSeleccionado.label);
+        if (productoExistenteIndex !== -1) {
+            // Si el producto ya existe, aumentar la cantidad en 1
+            const nuevosProductos = [...productosAgregados];
+            nuevosProductos[productoExistenteIndex].cantidad += 1;
+            nuevosProductos[productoExistenteIndex].valorTotal = nuevosProductos[productoExistenteIndex].cantidad * nuevosProductos[productoExistenteIndex].valorUnitario;
+            setProductosAgregados(nuevosProductos);
         } else {
-            setNombreError('');
-        }
-    
-        if (!data.apellido || !data.apellido.length) {
-            setApellidoeError('Campo requerido');
-            isValid = false;
-        } else {
-            setApellidoeError('');
-        }
-    
-        if (!data.email || !data.email.length) {
-            setEmailError('Campo requerido');
-            isValid = false;
-        } else {
-            setEmailError('');
-        }
-    
-        if (!data.password || !data.password.length) {
-            setPasswordError('Campo requerido');
-            isValid = false;
-        } else {
-            setPasswordError('');
-        }
-    
-        if (!data.confPassword || !data.confPassword.length) {
-            setConfPasswordError('Campo requerido');
-            isValid = false;
-        } else {
-            setConfPasswordError('');
-        }
-    
-        if (isValid) {
-            saveData(data);
+            // Si el producto no existe, agregarlo con cantidad 1
+            const nuevoProducto = {
+                referencia: productoSeleccionado.label,
+                producto: productoSeleccionado.label,
+                valorUnitario: productoSeleccionado.precio,
+                cantidad: 1,
+                valorTotal: productoSeleccionado.precio
+            };
+            setProductosAgregados([...productosAgregados, nuevoProducto]);
         }
     };
-    
+
+    const handleClick = () => {
+        // Tu lógica para redirigir al usuario
+    };
+
+    // Calcular el valor total general
+    const valorTotalGeneral = productosAgregados.reduce((total, producto) => total + producto.valorTotal, 0);
 
     return (
         <div className={styles.main_container}>
-            <Stack
-            direction='column'
-            alignItems="center"
-            justifyContent="center"
-            spacing={2}
-            sx={{
-                width: '100%',
-                height: '100vh',
-            }}
-            >
-                <Box sx={{paddingLeft: '1rem',width: "100%", display: 'flex', justifyContent: 'left', alignItems:'center' }}>
-                    <Box onClick={handleClick} sx={{cursor: 'pointer', display: 'flex', color: "#fff", justifyContent: "center", alignItems:"center"}}>
-                    <ArrowBackIosIcon id='backIcon' />
-                    <Typography variant='h6' > Atrás </Typography>
+            <Stack direction='column' alignItems="center" spacing={2} sx={{ width: '100%', height: '100vh' }}>
+                <Box sx={{ paddingLeft: '1rem', width: "100%", display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
+                    <Box onClick={handleClick} sx={{ cursor: 'pointer', display: 'flex', color: "#fff", justifyContent: "center", alignItems: "center" }}>
+                        <ArrowBackIosIcon id='backIcon' />
+                        <Typography variant='h6'> Atrás </Typography>
                     </Box>
                 </Box>
-                <Box
-                    sx={{
-                        bgcolor: 'white',
-                        
-                        borderRadius: '1rem',
-                        padding: '2rem 4rem',
-                        marginBottom: '1rem'
-                    }}
-                    xs={12}
-                    sm={12}
-                    md={10}
-                >
-                    <form onSubmit={handleSubmit(processForm)}>
-                        <Stack
-                            direction='column'
-                            alignItems="center"
-                            justifyContent="center"
-                            spacing={2}
-                            sx={{ height: '100%', padding: '1rem 0' }}
-                        >
-                            <Box sx={{ display: 'flex' }}>
-                                <Image src={vendorIcon} width={32} alt='worker' />
-                                <Typography sx={{
-                                    color: '#090069',
-                                    fontSize: '1.8rem',
-                                    marginLeft: '1rem',
-                                    fontWeight: 'bold',
-                                    marginBottom: '1rem'
-                                }}>
-                                    Registro Pedido
-                                </Typography>
-                            </Box>
-                            <TextField
-                                error={nombreError && nombreError.length ? true : false}
-                                autoFocus
-                                size="small"
-                                id="nombre"
-                                label="Nombre"
-                                variant="filled"
-                                {...register('nombre')}
-                                helperText={nombreError}
-                                sx={{ height: nombreError && nombreError.length ? 'auto' : '56px' }}
-
+                <Box sx={{ bgcolor: 'white', borderRadius: '1rem', padding: '2rem 4rem', marginBottom: '1rem' }}>
+                    <form>
+                        <Stack spacing={2} direction={'row'} s>
+                            <Autocomplete
+                                disablePortal
+                                id="combo-box-demo"
+                                options={Clientes}
+                                sx={{ width: 300 }}
+                                renderInput={(params) => <TextField {...params} label="Clientes" />}
                             />
-                            <TextField
-                                error={apellidoError && apellidoError.length ? true : false}
-                                size="small"
-                                id="apellido"
-                                label="Apellido"
-                                variant="filled"
-                                {...register('apellido')}
-                                helperText={apellidoError}
+                            <Button variant='contained'>Agregar Cliente</Button>
+                            <Autocomplete
+                                disablePortal
+                                id="combo-box-demo"
+                                options={Productos}
+                                sx={{ width: 300 }}
+                                renderInput={(params) => <TextField {...params} label="Productos" />}
+                                onChange={(event, value) => handleAddProducto(value)}
                             />
-                            <TextField
-                                error={emailError && emailError.length ? true : false}
-                                size="small"
-                                id="email"
-                                label="Correo Electrónico"
-                                variant="filled"
-                                {...register('email')}
-                                helperText={emailError}
-                            />
-                            <TextField
-                                error={passowordError && passowordError.length ? true : false}
-                                size="small"
-                                id="password"
-                                label="Contraseña"
-                                variant="filled"
-                                {...register('password')}
-                                helperText={passowordError}
-                            />
-                            <TextField
-                                error={confPasswordError && confPasswordError.length ? true : false}
-                                size="small"
-                                id="confPassowrd"
-                                label="Confirmar Contraseña"
-                                variant="filled"
-                                helperText={confPasswordError}
-                                {...register('confPassword')}
-                            />
-                            <Button
-                                type='submit'
-                                variant="contained"
-                                sx={{ backgroundColor: "#090069",
-                                "&:hover": {
-                                  backgroundColor: "#1d35f7",
-                                }, color: 'white' }}
-                            >
-                                Guardar
-                            </Button>
+                            <Typography variant='h6'>Total: ${valorTotalGeneral}</Typography>
+                            <Button variant='contained'>Generar Pedido</Button>
                         </Stack>
+                        <Divider sx={{ margin: "1rem 0" }} />
+                        {productosAgregados.map((producto, index) => (
+                            <Stack key={index} direction={"row"} spacing={2} sx={{ background: "#fff", padding: "1rem", margin: "1rem 0", borderRadius: "1rem" }}>
+                                <TextField value={producto.referencia} label="Referencia" />
+                                <TextField value={producto.producto} label="Producto" />
+                                <TextField value={producto.valorUnitario} label="Valor Unitario" />
+                                <TextField label="Cantidad" value={producto.cantidad} onChange={(event) => handleCantidadChange(event, index)} />
+                                <TextField value={producto.valorTotal} label="Valor Total" />
+                                <TextField label="Descuento %" />
+                                <Button variant='contained' onClick={() => handleDeleteProducto(index)}>Eliminar</Button>
+                            </Stack>
+                        ))}
                     </form>
                 </Box>
             </Stack>
         </div>
     );
 }
-
